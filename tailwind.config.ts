1a6746cb05129b802270c1cdc80611d9e6832125
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-07 16:19:39 +0700

    chore: add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "bg-dark": "#151c2c",
+                "bg-soft-dark": "#182237",
+                "active-dark": "#2e374a",
+                "text-dark": "white",
+                "text-soft-dark": "#b7bac1",
+
+                bg: "#151c2c",
+                "bg-soft": "#182237",
+                active: "#2e374a",
+                text: "white",
+                "text-soft": "#b7bac1",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
