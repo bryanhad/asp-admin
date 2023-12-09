b2847a40aefc166b785b1a4a54106e22cbec4f99
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-10 02:35:00 +0700

    chore: add little multiInput skeletons too! cuz like most likely those fields would be filled. so why not add the skeleton just for the sake of less jittering when the data finaly loads.

--- src/ui/skeletons/MemberFormSkeleton.tsx
+++ src/ui/skeletons/MemberFormSkeleton.tsx
@@ -4,14 +4,31 @@ import { InputSkeleton, TextSkeleton } from "./Skeleton"
 export default function MemberFormSkeleton() {
     return (
         <div className="flex flex-col gap-5 lg:flex-row lg:flex-wrap lg:justify-between">
-            <UploadPhotoSkeleton/>
+            <UploadPhotoSkeleton />
             <InputSkeleton containerClassName="w-full lg:w-[45%]" />
             <InputSkeleton containerClassName="w-full lg:w-[45%]" />
             <InputSkeleton containerClassName="w-full lg:w-[45%]" />
-            <InputSkeleton containerClassName="w-full lg:w-[45%]" />
-            <InputSkeleton containerClassName="w-full lg:w-[45%]" />
-            <InputSkeleton containerClassName="w-full lg:w-[45%]" />
-            <InputSkeleton containerClassName="w-full lg:w-[45%]" />
+            <div className="flex w-full flex-col gap-2 lg:w-[45%]">
+                <InputSkeleton containerClassName="" />
+                <div className="flex flex-wrap gap-3">
+                    <TextSkeleton circle className="h-[35px] w-[160px]" />
+                    <TextSkeleton circle className="h-[35px] w-[80px]" />
+                </div>
+            </div>
+            <div className="flex w-full flex-col gap-2 lg:w-[45%]">
+                <InputSkeleton containerClassName="" />
+                <div className="flex flex-wrap gap-3">
+                    <TextSkeleton circle className="h-[35px] w-[160px]" />
+                    <TextSkeleton circle className="h-[35px] w-[80px]" />
+                </div>
+            </div>
+            <div className="flex w-full flex-col gap-2 lg:w-[45%]">
+                <InputSkeleton containerClassName="" />
+                <div className="flex flex-wrap gap-3">
+                    <TextSkeleton circle className="h-[35px] w-[160px]" />
+                    <TextSkeleton circle className="h-[35px] w-[80px]" />
+                </div>
+            </div>
             <InputSkeleton containerClassName="w-full" className="h-[200px]" />
             {/* BUTTON */}
             <div className="w-full">
@@ -23,14 +40,19 @@ export default function MemberFormSkeleton() {
 
 function UploadPhotoSkeleton() {
     return (
-        <div className="flex gap-5 max-sm:max-w-[400px] max-w-[542px]">
+        <div className="flex max-w-[542px] gap-5 max-sm:max-w-[400px]">
             <div>
-            <TextSkeleton circle className="mx-auto w-[130px] h-[130px]" />
-
+                <TextSkeleton circle className="mx-auto h-[130px] w-[130px]" />
             </div>
-            <div className="flex-[1] flex flex-col items-center sm:flex-row gap-5">
-                <TextSkeleton circle className="min-w-[180px] w-full h-[50px]"/>
-                <TextSkeleton circle className="min-w-[180px] w-full h-[50px]"/>
+            <div className="flex flex-[1] flex-col items-center gap-5 sm:flex-row">
+                <TextSkeleton
+                    circle
+                    className="h-[50px] w-full min-w-[180px]"
+                />
+                <TextSkeleton
+                    circle
+                    className="h-[50px] w-full min-w-[180px]"
+                />
             </div>
         </div>
     )
