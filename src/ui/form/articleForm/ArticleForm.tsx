5f681e21f006702ab46a35be50174653955b604a
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-13 02:43:16 +0700

    feat: add tiptap (rich editor) skeleton, cuz it uses a hook and will be null at first. too make it less wonky and jittery, i made a simple seleton!

--- src/ui/form/articleForm/ArticleForm.tsx
+++ src/ui/form/articleForm/ArticleForm.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react"
 import { Button } from "../../shadcn/button"
 import Tiptap from "../../tiptap/Tiptap"
-import { createArticle } from "@/actions/articles.action"
 import Label from "../Label"
 import { useFormState } from "react-dom"
 import MyInput from "../MyInput"
