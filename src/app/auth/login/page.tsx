ce55a6a7233fe95027980bfd478c549608b144a2
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-13 21:53:56 +0700

    style add Logo to login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,17 +4,23 @@ import "@/ui/globals.css"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { redirect } from "next/navigation"
+import Logo from "@/ui/Logo"
 
 export default async function LoginPage() {
     const session = await getServerSession(authOptions)
 
     if (session) {
-        redirect('/')
+        redirect("/")
     }
     return (
         <div className="flex h-screen w-screen items-center justify-center bg-slate-100 px-4">
-            <div className="w-full max-w-[450px] space-y-10 rounded-xl bg-white px-8 pb-8 pt-12 shadow-xl">
-                <h1 className="text-2xl text-center font-semibold dark:text-black">Login</h1>
+            <div className="w-full max-w-[450px] space-y-8 rounded-xl bg-white px-8 pb-8 pt-12 shadow-xl">
+                <div className="flex justify-center">
+                    <Logo />
+                </div>
+                {/* <h1 className="text-center text-2xl font-semibold dark:text-black">
+                    Login
+                </h1> */}
                 <LoginForm />
             </div>
         </div>
