1055b132325d9f41799e3fcba259f49f533c95d5
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-13 20:54:29 +0700

    feat: logged in user that is in session cannot go to login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,16 @@
 import React from "react"
 import LoginForm from "./LoginForm"
 import "@/ui/globals.css"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    const session = await getServerSession(authOptions)
+
+    if (session) {
+        redirect('/')
+    }
     return (
         <div className="flex h-screen w-screen items-center justify-center bg-slate-100 px-4">
             <div className="w-full max-w-[450px] space-y-10 rounded-xl bg-white px-8 pb-8 pt-12 shadow-xl">
