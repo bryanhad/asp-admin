fcbb14254d0d49e5861f6f44ba09143c3417810b
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-13 19:44:38 +0700

    fix: add Dashboard title when on index page (home page with patname of '/'

--- src/ui/header/HeaderTitle.tsx
+++ src/ui/header/HeaderTitle.tsx
@@ -7,7 +7,7 @@ export default function HeaderTitle() {
     const pathnames = pathname.slice(1).split("/")
     return (
         <div className="flex gap-2 font-bold">
-            <div className="capitalize">{pathnames[0]}</div>
+            <div className="capitalize">{pathnames[0] ? pathnames[0] : 'Dashboard'}</div>
             {pathnames[2] && <div className="capitalize">{pathnames[2]}</div>}
         </div>
     )
