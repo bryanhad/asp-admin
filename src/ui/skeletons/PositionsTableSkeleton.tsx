6674ef839d2e6a8fa6a5f92e59fee26d8e8405b9
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-13 17:27:42 +0700

    feat: add positions table skeleton

--- src/ui/skeletons/PositionsTableSkeleton.tsx
+++ src/ui/skeletons/PositionsTableSkeleton.tsx
@@ -1,59 +1,60 @@
-import Table from "../tables/Table"
-import TableRowWithBorderBottom from "../tables/TableRowWithBorderBottom"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/ui/shadcn/table"
 import { TextSkeleton } from "./Skeleton"
 
-export default function PositionTableSkeleton() {
-    const numOfCols = Array.from(Array(2).keys())
-    const numOfDesktopRows = Array.from(Array(6).keys())
-    const numOfMobileRows = Array.from(Array(6).keys())
-
+export default function PositionsTableSkeleton() {
+    const numOfDesktopRows = Array.from(Array(2).keys())
     return (
-        <Table
-            theads={numOfCols.map((el) => (
-                <TextSkeleton key={el} className="w-[33%]" />
-            ))}
-            mobileView={
-                <>
-                    {numOfMobileRows.map((el) => (
-                        <div
-                            key={el}
-                            className="flex flex-col gap-4 rounded-lg bg-bg-soft p-4  dark:bg-bg-soft-dark"
-                        >
-                            <div className="flex justify-between">
-                                <TextSkeleton className="w-[150px]" />
-                                <TextSkeleton className="w-[30px]" />
-                            </div>
-                            <div className="flex gap-4">
-                                <TextSkeleton className="w-[80px]" />
-                                <TextSkeleton className="w-[80px]" />
-                            </div>
-                        </div>
-                    ))}
-                </>
-            }
-        >
-            <>
+        <Table>
+            <TableCaption>A list of recent positions.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[30%]">
+                        <TextSkeleton className="w-[60px]" />
+                    </TableHead>
+                    <TableHead>
+                        <TextSkeleton className="w-[60px]" />
+                    </TableHead>
+                    <TableHead className="flex items-center justify-end">
+                        <TextSkeleton className="w-[60px]" />
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
                 {numOfDesktopRows.map((el) => (
-                    <TableRowWithBorderBottom
-                        key={el}
-                        arrayLength={numOfDesktopRows.length}
-                        index={el}
-                    >
-                        <td className="p-4">
-                            {<TextSkeleton className="w-[50%]" />}
-                        </td>
-                        <td className="p-4">
-                            {<TextSkeleton className="w-[30px]" />}
-                        </td>
-                        <td className="p-4">
-                            <div className="ml-auto flex max-w-max gap-4">
-                                <TextSkeleton className="w-[80px]" />
-                                <TextSkeleton className="w-[80px]" />
-                            </div>
-                        </td>
-                    </TableRowWithBorderBottom>
+                    <TableRowSkeleton key={el} />
                 ))}
-            </>
+            </TableBody>
         </Table>
+    )
+}
+
+function TableRowSkeleton() {
+    return (
+        <TableRow>
+            <TableCell>
+                <div className="flex items-center gap-4">
+                    <TextSkeleton className="h-[18px] w-[100px]" />
+                </div>
+            </TableCell>
+            <TableCell>
+                <div className="flex items-center gap-4">
+                    <TextSkeleton className="h-[18px] w-[25px]" />
+                </div>
+            </TableCell>
+            <TableCell>
+                <div className="flex flex-col gap-4 max-sm:w-full sm:ml-auto sm:max-w-max sm:flex-row">
+                    <TextSkeleton className="w-full sm:w-[80px]" />
+                    <TextSkeleton className="w-full sm:w-[80px]" />
+                </div>
+            </TableCell>
+        </TableRow>
     )
 }
