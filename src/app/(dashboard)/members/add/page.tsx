6bc4049ef998d4aa069cafaeed8bfc7ade35ce77
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-14 14:31:08 +0700

    feat: add metadata to add pages

--- src/app/(dashboard)/members/add/page.tsx
+++ src/app/(dashboard)/members/add/page.tsx
@@ -1,6 +1,11 @@
 import { createMember } from "@/actions/members.action"
 import { prisma } from "@/lib/db/prisma"
 import MemberForm from "@/ui/form/memberForm/MemberForm"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Add Member',
+}
 
 export default async function AddMemberPage() {
     const positions = await prisma.position.findMany()
