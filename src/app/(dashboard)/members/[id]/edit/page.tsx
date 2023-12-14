2eae28f0246285ab50807abc29227ecad5955ab9
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-14 14:50:02 +0700

    feat: add dynamic metadata based on fetch, using generateMetadata

--- src/app/(dashboard)/members/[id]/edit/page.tsx
+++ src/app/(dashboard)/members/[id]/edit/page.tsx
@@ -1,6 +1,24 @@
 import React, { Suspense } from "react"
 import EditMemberForm from "./EditMemberForm"
 import MemberFormSkeleton from "@/ui/skeletons/form/MemberFormSkeleton"
+import { Metadata } from "next"
+import { prisma } from "@/lib/db/prisma"
+
+export async function generateMetadata({
+    params,
+}: {
+    params: { id: string }
+}): Promise<Metadata> {
+    const member = await prisma.member.findUnique({
+        where: { id: params.id },
+        select: { name: true, description: true },
+    })
+
+    return {
+        title: member?.name,
+        description: member?.description
+    }
+}
 
 export default async function EditMemberPage({
     params,
